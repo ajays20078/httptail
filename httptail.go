80434602167ed57ca3141d8061dcd2600409274c
smallfish <smallfish.xy@gmail.com>
2012-11-11 16:18:30 +0800

    apply redis multi-bulk protocol for subscribe

--- httptail.go
+++ httptail.go
@@ -4,11 +4,13 @@ package main
 import (
     "flag"
     "fmt"
-    "io"
     "net"
     "net/http"
     "os"
     "strings"
+    "strconv"
+    "io"
+    "bufio"
 )
 
 var mode string  // server or client
@@ -19,6 +21,20 @@ var bind string  // when mode equal server, bind httpserver host:port, default: 
 var redisPubProtocol = "*3\r\n$7\r\npublish\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n"
 var redisSubProtocol = "*2\r\n$9\r\nsubscribe\r\n$%d\r\n%s\r\n"
 
+type RedisConn struct {
+    conn net.Conn
+    buffer bufio.ReadWriter
+}
+
+func getRedisConn(address string) (conn *RedisConn, err error) {
+    var nc net.Conn
+    nc, err = net.Dial("tcp", address)
+    if err != nil {
+        return nil, err
+    }
+    return &RedisConn{conn: nc, buffer: bufio.ReadWriter{bufio.NewReader(nc), bufio.NewWriter(nc)}}, nil
+}
+
 func init() {
     flag.StringVar(&mode, "mode", "", "server or client")
     flag.StringVar(&redis, "redis", "0.0.0.0:6379", "redis host:port")
@@ -27,41 +43,45 @@ func init() {
 }
 
 func serverModeHandler(redis, bind string) {
-    // create default handler
     http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-        conn, err := net.Dial("tcp", redis)
+        rc, err := getRedisConn(redis)
         if err != nil {
-            fmt.Println("error: connect redis failed.", err)
+            fmt.Println("error:", err)
             return
         }
-        defer conn.Close()
+        defer rc.conn.Close()
         w.Header().Set("Content-Type", "text/html; charset=utf-8")
         io.WriteString(w, "httptail start...\n")
         w.(http.Flusher).Flush()
-        topic := req.URL.Path[1:]
-        _, err = conn.Write([]byte(fmt.Sprintf(redisSubProtocol, len(topic), topic)))
-        if err != nil {
+        topic   := req.URL.Path[1:]
+        command := fmt.Sprintf(redisSubProtocol, len(topic), topic)
+        if _, err := rc.buffer.WriteString(command); err != nil {
             fmt.Println("error: subscribe redis failed.", err)
             return
         }
-        var buf = make([]byte, 1024)
+        rc.buffer.Flush()
         for {
-            n, err := conn.Read(buf)
-            if err == io.EOF {
-                break
+            line, prefix, err := rc.buffer.ReadLine()
+            if prefix || err != nil {
+                fmt.Println("error:", err)
+                return
             }
-            if n > 0 {
-                lines := strings.Split(string(buf[0:n]), "\r\n")
-                array := make([]string, 0)
-                for i := 0; i < len(lines); i++ {
-                    if strings.Index(lines[i], "$") == 0 { // ignore *
-                        array = append(array, lines[i+1])
+            resp := string(line)
+            if strings.HasPrefix(resp, "*") {
+                bulk, _ := strconv.ParseInt(resp[1:], 10, 0)
+                row  := make([]string, 0)
+                for i:=0; i<int(bulk); i++ {
+                    line, _, _ := rc.buffer.ReadLine()
+                    resp := string(line)
+                    if strings.HasPrefix(resp, "$") {
+                        count, _ := strconv.ParseInt(resp[1:], 10, 0)
+                        buf := make([]byte, count+2)
+                        io.ReadFull(rc.buffer, buf)
+                        row = append(row, string(buf[:count]))
                     }
                 }
-                if array[0] == "message" { // ignore subscribe
-                    io.WriteString(w, fmt.Sprintf("%s %s", array[1], array[2]))
-                    w.(http.Flusher).Flush()
-                }
+                io.WriteString(w, strings.Join(row[1:], " "))
+                w.(http.Flusher).Flush()
             }
         }
     })
@@ -74,12 +94,12 @@ func serverModeHandler(redis, bind string) {
 }
 
 func clientModeHandler(redis, topic string) {
-    conn, err := net.Dial("tcp", redis)
+    rc, err := getRedisConn(redis)
     if err != nil {
-        fmt.Println("error: connect redis failed.", err)
+        fmt.Println("error:", err)
         return
     }
-    defer conn.Close()
+    defer rc.conn.Close()
     buf := make([]byte, 1024)
     for {
         n, err := os.Stdin.Read(buf)
@@ -89,11 +109,11 @@ func clientModeHandler(redis, topic string) {
         if n > 0 {
             value := string(buf[0:n])
             command := fmt.Sprintf(redisPubProtocol, len(topic), topic, len(value), value)
-            _, err := conn.Write([]byte(command))
-            if err != nil {
-                fmt.Println("error: publish to redis failed.", err)
+            if _, err := rc.buffer.WriteString(command); err != nil {
+                fmt.Println("error: publish redis failed.", err)
                 return
             }
+            rc.buffer.Flush()
             fmt.Print(value)
         }
     }
